c1e0a50bffd7b006cb95c8f97ee8ad1129cbff22
Peter (Petie) Sefton <pt@ptsefton.com>
2015-10-16 08:16:52 +1100

    Can now parse some sample chords using the http://ukegeeks.com format  flavour of chordpro

--- chorddiagram.py
+++ chorddiagram.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageFont, ImageDraw
+import re
 class Dot:
     """ Class to represent a single dot in the diagram ie a finger on a fret"""
     def __init__(self, fret, finger = None):
@@ -25,9 +26,9 @@ class ChordDiagram:
     top_margin = 5 # Between chord name and zero fret
     bottom_margin = 5 #between last fret and bottom of diagram
     box_size = (box_width, box_height)
-    bgcolor = (255,255,255)
-    dot_text_color = (256,256,256)
-    dot_color = (256,0,256)
+    bgcolor = (255,255,255) #whitish
+    dot_text_color = (256,256,256) #white
+    dot_color = (0,0,0) #black
     
     def __init__(self, name="C7", strings=[String([Dot(0)]),String([Dot(0)]),String([Dot(0)]),String([Dot(1, 1)])]):
         """ Defaults to a diagram for a sprano uke C7 chord """
@@ -46,7 +47,8 @@ class ChordDiagram:
         w, h = draw.textsize(self.name)
         draw.text(((ChordDiagram.box_width - w) / 2, 0),self.name, (0,0,0))
         
-        # Draw plenty of strings evenly placed across the diagram
+        # Draw plenty of strings evenly placed across the diagram, instrument agnostic,
+        # Just draw all the strings
         self.string_top = h + ChordDiagram.top_margin
         self.string_bottom = ChordDiagram.box_height - ChordDiagram.bottom_margin
         self.num_strings = len(self.strings)
@@ -62,11 +64,11 @@ class ChordDiagram:
         self.fret_spacing = (self.string_bottom - self.string_top) / self.num_frets
         
         for i in range(0, self.num_frets + 1):
-            fret = Fret(self.string_spacing, self.string_top +  i * self.fret_spacing, self.string_spacing * self.num_strings )
+            fret = Fret(self.string_spacing, self.string_top +  i * self.fret_spacing, self.string_spacing * self.num_strings)
             self.frets.append(fret)
             draw.line(( fret.left_x, fret.y, fret.right_x, fret.y ), fill=128)
             
-        # Draw the dots
+        # Draw the dots which are stored by string
         for i in range(0, len(self.strings)):
             string = self.strings[i]
             # TODO deal with dont_play
@@ -79,11 +81,63 @@ class ChordDiagram:
                 if dot.fret != 0:
                     draw.ellipse((x-r, y-r, x+r, y+r), ChordDiagram.dot_color)
                     if dot.finger != None:
-                        draw.text((x - r /3.1415 ,y - r ),str(dot.finger), ChordDiagram.dot_text_color)
+                        #This maths is a result of trial and error
+                        #Duh! Centre on fret and string ! Forget R
+                        draw.text((x - r /3.1415 ,y - r ),
+                                  str(dot.finger),
+                                  ChordDiagram.dot_text_color)
                     
     def show(self):
         self.draw()
         self.img.show()
+
+
+    def parse_definition(self, definition):
+        """ unpack a chordpro chord definition, trying to be as permissive as possible """
+        frets_re = re.compile("{define: +(.*?) *(frets)? +([\\d ]+)", re.IGNORECASE)
+        frets_search = re.search(frets_re, definition)
+        print(frets_search)
+        if frets_search != None:
+            #Get rid of basic frets part
+            definition = re.sub(frets_re, "", definition)
+            
+            #Look for optional fingers spec
+            print("LOOKING HERE", definition)
+            fingers_re = re.compile("fingers +([\\d ]+)", re.IGNORECASE)
+            fingers_search = re.search(fingers_re, definition)
+            fingers = None
+            if fingers_search != None:
+                fingers = fingers_search.group(1).strip().split(" ")
+                definition = re.sub(fingers_re, "", definition)
+                    
+            self.strings = []
+            frets = frets_search.group(3).strip().split(" ")
+            self.name = frets_search.group(1)
+            i = 0
+            for fret in frets:
+                f_int = int(fret)
+                finger = None
+                if fingers != None:
+                    finger = fingers[i] if fingers[i] != 0 else None
+                                                        
+                self.strings.append(String([Dot(int(fret), finger)]))
+                i += 1
+             # Look for additional fingers
+            definition = definition.replace("}","")
+            additional = definition.strip().split("add: ")
+            for dot_to_add in additional:
+                 add_re = re.compile("string +(\d+) +fret +(\d+) +finger +(\d+)")
+                 add_search = re.search(add_re, dot_to_add)
+                 if add_search != None:
+                    print(add_search)
+                    string = int(add_search.group(1)) - 1
+                    fret = int(add_search.group(2))
+                    finger = int(add_search.group(3))
+                    print(string, fret, finger)
+                    if string <= len(frets) and fret > 0:
+                        self.strings[string].dots.append(Dot(fret, finger))
+                 
+        
 
 
         
--- test_chorddiagram.py
+++ test_chorddiagram.py
@@ -34,20 +34,76 @@ class TestChorddiagram(unittest.TestCase):
 
     
     d= cd.ChordDiagram( name="G", strings=[String([Dot(0)]),String([Dot(2,1)]),String([Dot(3,3)]),String([Dot(2, 2)])])
-    #d.show()
+    d.draw()
 
     #Guitar G chord
     d= cd.ChordDiagram( name="G", strings=[String([Dot(3,2)]),String([Dot(2,1)]),String([Dot(0)]),String([Dot(0)]),String([Dot(0)]), String([Dot(3,3)])])
     #Check strings are not on top of each other
    
     
-    d.show()
+    d.draw()
     self.assertEqual(d.num_strings, 6)
     string_x = 0
     for s in d.strings:
       self.assertTrue(s.string_x > string_x)
       string_x = s.string_x
       self.assertTrue(string_x < cd.ChordDiagram.box_width)
+
+  def test_parse(self):
+        #Simple chord
+        achord = "{define: A frets 2 1 0 0}"
+        d = cd.ChordDiagram()
+        d.parse_definition(achord)
+        d.draw()
+        self.assertEqual(d.num_strings, 4)
+        self.assertEqual(d.strings[0].dots[0].fret, 2)
+        self.assertEqual(d.strings[0].dots[0].finger, None)
+        self.assertEqual(d.strings[1].dots[0].fret, 1)
+        self.assertEqual(d.strings[1].dots[0].finger, None)
+        self.assertEqual(d.strings[2].dots[0].fret, 0)
+        self.assertEqual(d.strings[2].dots[0].finger, None)
+        self.assertEqual(d.strings[3].dots[0].fret, 0)
+        self.assertEqual(d.strings[3].dots[0].finger, None)
+        
+        # Simple chord without "frets"
+        achord = "{define: A 2 1 0 0}"
+        d = cd.ChordDiagram()
+        d.parse_definition(achord)
+        d.draw()
+        self.assertEqual(d.num_strings, 4)
+        self.assertEqual(d.strings[0].dots[0].fret, 2)
+        self.assertEqual(d.strings[0].dots[0].finger, None)
+        self.assertEqual(d.strings[1].dots[0].fret, 1)
+        self.assertEqual(d.strings[1].dots[0].finger, None)
+        self.assertEqual(d.strings[2].dots[0].fret, 0)
+        self.assertEqual(d.strings[2].dots[0].finger, None)
+        self.assertEqual(d.strings[3].dots[0].fret, 0)
+        self.assertEqual(d.strings[3].dots[0].finger, None)
+        
+        # Chord with fingering
+        achord = "{define: A frets 2 1 0 0 fingers 2 1 0 0}"
+        d = cd.ChordDiagram()
+        d.parse_definition(achord)
+        d.draw()
+        self.assertEqual(d.num_strings, 4)
+        self.assertEqual(d.strings[0].dots[0].fret, 2)
+        self.assertEqual(d.strings[1].dots[0].fret, 1)
+        self.assertEqual(d.strings[2].dots[0].fret, 0)
+        self.assertEqual(d.strings[3].dots[0].fret, 0)
+
+        
+        # Chord with added fingering
+        aaugchord = "{define: Aaug frets 2 1 1 4 fingers 2 1 1 4 add: string 1 fret 1 finger 1 add: string 4 fret 1 finger 1}"
+        d = cd.ChordDiagram()
+        d.parse_definition(aaugchord)
+        d.show()
+        self.assertEqual(d.num_strings, 4)
+        # Chord starting on a higher fret
+
+
+        # Chord with non-played strings
+
+      
 
 if __name__ == '__main__':
     unittest.main()
