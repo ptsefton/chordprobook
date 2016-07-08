b5767546ec2b83e191cd5d9f43af14b698cdce26
Peter Sefton <peter.sefton@uts.edu.au>
2016-07-08 17:16:50 +1000

    Make this installable as a library

---
+++ setup.py
@@ -0,0 +1,38 @@
+# chardet's setup.py
+from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
+setup(
+    name = "chorprobook",
+    packages = ["chordprobook"],
+    package_data={   
+   'chordprobook': ['instruments.yaml','chord_data/*.cho'] 
+     },
+    version = "0.1",
+    description = "Chordpro songsheet and book generator",
+    
+    author = "Peter Sefton",
+    author_email = "pt@ptsefton.com",
+    url = "https://github.com/ptsefton/chordprobook",
+    download_url = "https://github.com/ptsefton/chordprobook",
+    keywords = ["chordpro", "pdf", "song"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GPL",
+        "Operating System :: OS Independent",
+    
+        ],
+    long_description = """\
+This is a Python 3 script to convert collections of chordpro formatted song charts to 
+PDF, HTML, epub, and word processing doc formats including chord diagrams.
+
+Can convert a directory full of files to a single book, or a set of song-sheets, also handles setlists. 
+
+Uses Pandoc and wkhtmltopdf to do all the hard work.
+"""
+)
